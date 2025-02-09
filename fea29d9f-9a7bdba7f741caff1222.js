5ba3466e7ca1eb3994d4373f9954715b1db445ed
juanpablor <juanpablor@users.noreply.github.com>
2025-02-09 14:23:02 +0000

    deploy: juanpablor/planchersrenovpc.ca@011990be77326d3b326fccb41c45d812625b82dc

---
+++ fea29d9f-9a7bdba7f741caff1222.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkplanchersrenovpc=self.webpackChunkplanchersrenovpc||[]).push([[687],{9879:function(t,c,n){n.d(c,{mm2:function(){return e}});var a=n(2225);function e(t){return(0,a.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"},child:[]},{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=fea29d9f-9a7bdba7f741caff1222.js.map
